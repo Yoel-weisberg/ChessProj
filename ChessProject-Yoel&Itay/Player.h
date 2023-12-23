15fa2e917edf1a51322ed5fe22daba04e303ed7a
Itay Shalev <itayshallev@gmail.com>
2023-12-23 03:11:58 +0200

    Created a header file for 'Player' class

--- ChessProject-Yoel&Itay/Player.h
+++ ChessProject-Yoel&Itay/Player.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#define WHITE "white"
+#define BLACK "black"
+#define EMPTY "empty"
+
+
 class Player
 {
+public:
+	Player(const std::string& playerColor);
 
+	std::string getPlayerColor() const;
+	bool isEmpty() const;
+
+	bool operator==(const Player& other) const;
+
+private:
+	std::string _playerColor;
 };
