8f15b735d742e6e347047cc7c2e67e953b1f2fc9
Itay Shalev <itayshallev@gmail.com>
2023-12-23 05:41:27 +0200

    Updated VS Project: Renamed 'Board' (static functions class) to 'BoardUtils' and added a new 'Board' class

---
+++ ChessProject-Yoel&Itay/Board.cpp
@@ -0,0 +1,7 @@
+#include "Board.h"
+
+
+Board::Board()
+{
+	// Build the Chess Board
+}
---
+++ ChessProject-Yoel&Itay/BoardUtils.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Piece.h"
+#include "Point.h"
+#include "Player.h"
+
+#define ROWS 8
+#define COLS 8
+
+
+enum returnCode
+{
+	VALID_MOVE = 0,						// Valid move
+	CHECK_MOVE = 1,						// Valid move
+	MISSING_CURRENT_IN_SRC = 2,			// Invalid move
+	DST_OCCUPIED_BY_CURRENT = 3,		// Invalid move
+	CAUSES_CHECK_ON_CURRENT = 4,		// Invalid move
+	INVALID_INDEXES = 5,				// Invalid move
+	ILLEGAL_MOVE_FOR_PIECE = 6,			// Invalid move
+	SAME_SRC_DST_POINTS = 7,			// Invalid move
+//	CHECKMATE_MOVE = 8					// Valid move
+};
+
+
+class BoardUtils
+{
+public:
+	static Player getPointPlayer(const Piece* (&board)[ROWS][COLS], const Point& point);
+
+	static bool isPointInBoundaries(const Point& point);
+	static bool isKingInCheck(const Piece* (&board)[ROWS][COLS], const Player& player);
+	static returnCode isMoveValid(const Piece* (&board)[ROWS][COLS], const Player& turn, const Point& src, const Point& dst);
+
+	static bool movePiece(Piece* (&board)[ROWS][COLS], const Point& src, const Point& dst);
+
+	static void printBoard(const Piece* (&board)[ROWS][COLS]);
+};
