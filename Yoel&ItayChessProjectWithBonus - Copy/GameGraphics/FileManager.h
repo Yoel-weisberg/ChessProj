85b82c0ab2c77ad848727c562a487a248f00f8c2
Yoel_Weisberg5 <thek57525@gmail.com>
2024-01-03 23:28:40 +0200

    deleted copy project

--- Yoel&ItayChessProjectWithBonus - Copy/GameGraphics/FileManager.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <fstream>
-class FileManage
-{
-
-};
--- Yoel&ItayChessProjectWithBonus - Copy/GameLogic/FileManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <fstream>
-#include "Game.h"
-class FileManage
-{
-public:
-	static Game* openGameFromFile(const std::string& fileName);
-	static bool saveGameIntoFile(const Game& game, const std::string& name);
-};
